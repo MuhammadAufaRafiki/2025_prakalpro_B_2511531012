6e205927d4a21055675015861b20f43f0a5b8bda
HP <HP@LAPTOP-NTBE5G5F>
2025-09-21 22:45:06 +0700

    pekan3

---
+++ 2025_prakalpro_B_2511531012/src/pekan3/OperatorAritmatika.java
@@ -0,0 +1,36 @@
+package pekan3;
+
+import java.util.Scanner;
+
+public class OperatorAritmatika {
+
+	public static void main(String[] args) {
+		int A1;
+		int A2;
+		int hasil;
+		Scanner keybord = new Scanner (System.in);
+		System.out.print("Input angka-1:");
+		A1 = keybord.nextInt();
+		System.out.print("Input anhka-2:");
+		A2 = keybord.nextInt();
+		keybord.close();
+		System.out.println("operator Penjumlahan");
+		hasil = A1 + A2; // penjumlahan 
+		System.out.println("Hasil = " + hasil);
+		System.out.println("operator Pengurangan");
+		hasil = A1 - A2; // pengurangan
+		System.out.println("Hasil = " + hasil);
+		System.out.println("operator perkalian");
+		hasil = A1 * A2; // perkalian
+		System.out.println("Hasil = " + hasil);
+		System.out.println("operator hasil bagi");
+		hasil = A1 / A2; // hasil bagi
+		System.out.println("Hasil = " + hasil);
+		System.out.println("operator sisa bagi");
+		hasil = A1 % A2; // sisa bagi
+		System.out.println("Hasil = " + hasil);
+
+	}
+
+}
+
---
+++ 2025_prakalpro_B_2511531012/src/pekan3/OperatorAssignment.java
@@ -0,0 +1,34 @@
+package pekan3;
+
+import java.util.Scanner;
+
+public class OperatorAssignment {
+
+	public static void main(String[] args) {
+		int A1;
+		int A2;
+		Scanner keybord = new Scanner (System.in);
+		System.out.print("Input angka-1:");
+		A1 = keybord.nextInt();
+		System.out.print("Input anhka-2:");
+		A2 = keybord.nextInt();
+		keybord.close();
+		System.out.println("Assignment penambahan");
+	    A1+= A2; // penambahan, sekarang A1=15 
+		System.out.println("penambahan = " + A1);
+		System.out.println("Assignment Pengurangan");
+		A1 -= A2; // pengurangan, sekarang A1= 10
+		System.out.println("pengurangan = " + A1);
+		System.out.println("Assignment perkalian");
+		A1 *= A2; // perkalian, sekarang A1= 50
+		System.out.println("Hasil = " + A1);
+		System.out.println("Assignment hasil bagi");
+		A1 /= A2; // hasil bagi, sekarang A1=10
+		System.out.println("hasil bagi = " + A1);
+		System.out.println("Assignment sisa bagi");
+		A1 %= A2; // sisa bagi, sekarang A1=0
+		System.out.println("sisa bagi = " + A1);
+
+	}
+
+}
