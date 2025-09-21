81c5fc7614cc96939091e152de8b3beca1636abe
HP <HP@LAPTOP-NTBE5G5F>
2025-09-21 07:26:27 -0400

    tugasAlproPekan2

--- 2025_prakalpro_B_2511531012/src/tugasAlproPekan2/tugasAlproPekan2.java
+++ 2025_prakalpro_B_2511531012/src/tugasAlproPekan2/tugasAlproPekan2.java
@@ -3,8 +3,18 @@ package tugasAlproPekan2;
 public class tugasAlproPekan2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Deklarasi variabel data mahasiswa
+        int nim = 11531012;           // nim mahasiswa
+        float ipk = 3.57f;            // nilai ipk mahasiswa (pakai f untuk float)
+        char kelas = 'B';             // kelas mahasiswa
+        boolean aktif = true;         // status aktif mahasiswa (true = aktif , false = tidak)
 
+        // Output data ke layar
+        System.out.println("=== Data Mahasiswa ===");
+        System.out.println("NIM        : " + nim);
+        System.out.println("IPK        : " + ipk);
+        System.out.println("Kelas      : " + kelas);
+        System.out.println("Status Aktif: " + aktif);
 	}
-
+	
 }
