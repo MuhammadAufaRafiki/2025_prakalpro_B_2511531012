b6f7ce20fee33af81fa963f6a98e44706dbf419f
HP <HP@LAPTOP-NTBE5G5F>
2025-11-03 11:42:03 +0700

    pekan6_2511531012

---
+++ 2025_prakalpro_B_2511531012/src/pekan6_2511531012/SentinelLoop_2511531012.java
@@ -0,0 +1,21 @@
+package pekan6_2511531012;
+
+import java.util.Scanner;
+
+public class SentinelLoop_2511531012 {
+
+	public static void main(String[] args) {
+		Scanner console = new Scanner (System.in);
+		int sum = 0;
+		int number=12;
+		
+		while (number !=0) {
+			System.out.print("Maasukkan angka (0 untuk keluar):");;
+			number = console.nextInt();
+			sum = sum + number;
+		}
+		System.out.println("totalnya adalah " + sum);
+
+	}
+
+}
