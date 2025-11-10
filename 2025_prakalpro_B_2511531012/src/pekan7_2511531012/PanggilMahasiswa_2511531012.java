e17d7ac81831c8fa30a2798c74fecc7dec95896b
HP <HP@LAPTOP-NTBE5G5F>
2025-11-10 11:40:08 +0700

    pekan7_2511531012

---
+++ 2025_prakalpro_B_2511531012/src/pekan7_2511531012/PanggilMahasiswa_2511531012.java
@@ -0,0 +1,15 @@
+package pekan7_2511531012;
+
+public class PanggilMahasiswa_2511531012 {
+
+	public static void main(String[] args) {
+		Mahasiswa_2511531012 a= new Mahasiswa_2511531012();
+		a.setNim(1531012);
+		a.setNama("Muhammad Aufa Rafiki");
+		System.out.println(a.getNim());
+		System.out.println(a.getNama());
+		a.Cetak();
+
+	}
+
+}
---
+++ 2025_prakalpro_B_2511531012/src/pekan7_2511531012/String1_2511531012.java
@@ -0,0 +1,14 @@
+package pekan7_2511531012;
+
+public class String1_2511531012 {
+
+	public static void main(String[] args) {
+		String salam = "Assalamualaikum";
+		System.out.println("panjang salam adalah: " + salam.length());
+		System.out.println(salam.toUpperCase());  //output "Assalamualaikum"
+		System.out.println(salam.toLowerCase());  //output "assalamualaikum"
+		System.out.println(salam.indexOf("salam")); // outputs 2
+
+	}
+
+}
